62f8ade77532ae194aa5fdf56baa2fd434f2e87e
Julien Hauseux <julien.hauseux@gmail.com>
2024-12-19 12:51:38 +0100

    refactor(schema): rename column key to value

---
+++ hasura/migrations/default/1734608824092_rename_key_value/up.sql
@@ -0,0 +1,8 @@
+ALTER TABLE fonction
+    RENAME COLUMN key TO value;
+
+ALTER TABLE type_enseignement
+    RENAME COLUMN key TO value;
+
+ALTER TABLE type_modification_service
+    RENAME COLUMN key TO value;
