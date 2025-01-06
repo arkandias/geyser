77a0306be9852bae04c7120f356e08b7ed7009b5
Julien Hauseux <julien.hauseux@gmail.com>
2025-01-06 15:16:08 +0100

    refactor: remove uid column in demande, priorite, message, and replace with service_id

--- hasura/migrations/default/1734688604936_check_service_request/down.sql
+++ hasura/migrations/default/1734688604936_check_service_request/down.sql
@@ -1,15 +1,13 @@
 DROP TRIGGER IF EXISTS check_demande_service ON demande;
 DROP FUNCTION IF EXISTS check_demande_service();
 
-DROP TRIGGER IF EXISTS check_priorite_service ON demande;
+DROP TRIGGER IF EXISTS check_priorite_service ON priorite;
 DROP FUNCTION IF EXISTS check_priorite_service();
 
 CREATE OR REPLACE FUNCTION calcul_anciennetes(annee integer) RETURNS void AS
 $$
 INSERT INTO priorite (uid, ens_id, anciennete)
-SELECT d.uid,
-       e.id,
-       coalesce(p.anciennete + 1, 1)
+SELECT d.uid, e.id, coalesce(p.anciennete + 1, 1)
 FROM enseignement e
          JOIN demande d ON d.ens_id = e.parent_id
          LEFT JOIN priorite p ON d.uid = p.uid AND e.parent_id = p.ens_id
