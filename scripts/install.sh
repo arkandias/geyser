9791d24c3df2171699a257a1e7e08c639e7e83d9
Julien Hauseux <julien.hauseux@gmail.com>
2025-01-18 23:05:15 +0100

    Refactor Geyser structure and improve database handling

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-# Exit the script immediately if any command fails
-set -e
-
 download() {
     if command -v curl >/dev/null 2>&1; then
-        curl -fsSL -o geyser-tmp.zip "$1"
+        curl -fsSL -o "$1" "$2"
     elif command -v wget >/dev/null 2>&1; then
-        wget -qO geyser-tmp.zip "$1"
+        wget -qO "$1" "$2"
     else
         echo >&2 "Error: No download tool available. Please install curl or wget to continue."
         exit 1
@@ -16,10 +13,12 @@ download() {
 
 if [ -n "${GEYSER_VERSION}" ]; then
     # If GEYSER_VERSION is set (not empty), download the specific tag
+    zipname="geyser-${GEYSER_VERSION}.zip"
     url="https://github.com/arkandias/geyser-backend/archive/refs/tags/${GEYSER_VERSION}.zip"
 else
     # If GEYSER_VERSION is not set, fallback to the master branch
-    url="https://github.com/arkandias/geyser-backend/archive/refs/heads/master.zip"
+    zipname="geyser.zip"
+    url="https://github.com/arkandias/docker-nordlynx/archive/refs/heads/master.zip"
 fi
 
 # Download the GitHub repository
@@ -29,16 +28,17 @@ download "${url}" || {
 }
 
 # Extract the downloaded zip file
-tar -xfv geyser-tmp.zip || {
+install_dir="$(tar -tf geyser.zip | head -n1)"
+tar -xf geyser.zip || {
     echo "Extraction failed" >&2
     exit 1
 }
 
 # Remove the zip file
-rm geyser-tmp.zip || {
+rm geyser.zip || {
     echo "Cleaning failed" >&2
     exit 1
 }
 
 echo "Successfully installed Geyser ${GEYSER_VERSION}"
-echo "Navigate to the installation directory with: cd geyser-backend-${GEYSER_VERSION##v}"
+echo "Navigate to the installation directory with: cd ${install_dir}"
