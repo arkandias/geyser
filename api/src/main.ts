152fbec0c09560f1299d38e31c1cde3c1ee70a69
Julien Hauseux <julien.hauseux@gmail.com>
2025-05-14 18:45:27 +0200

    WIP

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,3 +1,4 @@
+import { Logger } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import cookieParser from "cookie-parser";
 
@@ -7,19 +8,22 @@ import { ConfigService } from "./config/config.service";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const logger = new Logger("Bootstrap");
   const configService = app.get(ConfigService);
 
+  const allowOrigin =
+    configService.nodeEnv === "production" ? configService.api.origin : "*";
+  logger.log(`CORS enabled: ${allowOrigin}`);
   app.enableCors({
-    origin:
-      configService.nodeEnv === "production" ? configService.api.origin : "*",
+    origin: allowOrigin,
     credentials: true,
   });
 
   app.use(cookieParser());
 
   await app.listen(configService.port);
-  console.log(
-    `Application is running on: http://localhost:${configService.port}`,
+  logger.log(
+    `Server running at http://localhost:${configService.port} (${configService.nodeEnv})`,
   );
 }
 
