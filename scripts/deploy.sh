c855183a2d8cfbcdc36a05f230db5b2df917638a
Julien Hauseux <julien.hauseux@gmail.com>
2025-05-16 08:59:41 +0200

    WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# This script is a wrapper to be used by the webhook.
 
 SCRIPT_DIR="$(cd -- "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
+
 "${SCRIPT_DIR}/geyser" deploy
