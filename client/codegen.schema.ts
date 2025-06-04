eaa75408a126d44e988b023409dddd867b725afb
Julien Hauseux <julien.hauseux@gmail.com>
2025-06-04 09:26:22 +0200

    WIP

--- client/codegen.schema.ts
+++ client/codegen.schema.ts
@@ -3,19 +3,24 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.development" });
 
-const graphqlUrl = process.env["VITE_GRAPHQL_URL"];
-const apiAdminSecret = process.env["API_ADMIN_SECRET"];
-
-if (!graphqlUrl) {
-  throw new Error("Missing VITE_GRAPHQL_URL environment variable");
+let graphqlUrl: URL;
+if (process.env["VITE_GRAPHQL_URL"]) {
+  graphqlUrl = new URL(process.env["VITE_GRAPHQL_URL"]);
+} else if (process.env["VITE_API_URL"]) {
+  const apiUrl = new URL(process.env["VITE_API_URL"]);
+  graphqlUrl = new URL("/graphql", apiUrl.origin);
+} else {
+  throw new Error("Either VITE_API_URL or VITE_GRAPHQL_URL must be set");
 }
+
+const apiAdminSecret = process.env["API_ADMIN_SECRET"];
 if (!apiAdminSecret) {
   throw new Error("Missing API_ADMIN_SECRET environment variable");
 }
 
 const config: CodegenConfig = {
   schema: {
-    [graphqlUrl]: {
+    [graphqlUrl.href]: {
       headers: {
         "X-Admin-Secret": apiAdminSecret,
       },
