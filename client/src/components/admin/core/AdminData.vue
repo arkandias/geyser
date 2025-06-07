4e17eb0bbdd6390f30219e68d0d73d89a0d8c79a
Julien Hauseux <julien.hauseux@gmail.com>
2025-06-07 12:06:18 +0200

    eslint

--- client/src/components/admin/core/AdminData.vue
+++ client/src/components/admin/core/AdminData.vue
@@ -403,6 +403,7 @@ const filterMethod = (
           const col = cols.find((col) => col.name === filter.name);
           return (
             !col ||
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
             filterValues.value[filter.name]?.includes(getField(row, col.field))
           );
         })),
