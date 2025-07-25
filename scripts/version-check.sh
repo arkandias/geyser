f70a28a6384f617c31782e10d0fff205149568b7
Julien Hauseux <julien.hauseux@gmail.com>
2025-07-25 16:23:57 +0200

    Fix(scripts): add version consistency check to test script

---
+++ scripts/version-check.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Exit the script immediately if any command fails
+set -e
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}")")" &>/dev/null && pwd)"
+
+V_PACKAGE=$(awk -F'"' '/"version"/ {print $4}' "${SCRIPT_DIR}/../package.json")
+V_INSTALL=$(grep 'GEYSER_VERSION=' "${SCRIPT_DIR}/install.sh" | cut -d'"' -f2)
+V_GEYSER=$(grep 'GEYSER_VERSION=' "${SCRIPT_DIR}/geyser" | cut -d'"' -f2)
+
+if [ "${V_PACKAGE}" = "${V_INSTALL}" ] && [ "${V_PACKAGE}" = "${V_GEYSER}" ]; then
+    echo "All versions match (${V_PACKAGE})"
+else
+    echo "Version mismatch: package.json=${V_PACKAGE}, install.sh=${V_INSTALL}, geyser=${V_GEYSER}"
+    exit 1
+fi
