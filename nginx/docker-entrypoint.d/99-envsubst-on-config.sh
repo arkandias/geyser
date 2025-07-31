e46e834a2431e857790e621bda56a20a0be48a71
Julien Hauseux <julien.hauseux@gmail.com>
2025-07-31 15:38:44 +0200

    Chore(nginx, client): refactor `config.json` handling; update templates and entrypoint for consistency and cleanup

--- nginx/docker-entrypoint.d/99-envsubst-on-config.sh
+++ nginx/docker-entrypoint.d/99-envsubst-on-config.sh
@@ -13,5 +13,5 @@ ME=$(basename "$0")
 # Generate runtime configuration file by substituting environment variables
 # Reads template file with placeholder variables and replaces them with actual
 # environment variable values to create the final config
-envsubst </usr/share/nginx/html/config.template.json >/usr/share/nginx/html/config.json
+envsubst </usr/share/nginx/html/config.json.template >/usr/share/nginx/html/config.json
 entrypoint_log "$ME: Generated config.json from template with environment variables"
