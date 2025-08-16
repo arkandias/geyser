bd10e205e85306d4114d03a7217762755bb42aff
Julien Hauseux <julien.hauseux@gmail.com>
2025-08-16 12:04:17 +0200

    Chore(constants): update `DEFAULT_ORGANIZATION_KEY` value from "master" to "default"

--- shared/src/constants.ts
+++ shared/src/constants.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_ORGANIZATION_KEY = "master";
+export const DEFAULT_ORGANIZATION_KEY = "default";
 export const MASTER_ORGANIZATION_KEY = "master";
 export const MASTER_ORGANIZATION_ID = 0;
 export const ADMIN_USER_ID = 0;
