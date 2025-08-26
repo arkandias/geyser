1018aece94a22c6df1e72c5ff79a528e186a36d9
Julien Hauseux <julien.hauseux@gmail.com>
2025-08-26 19:38:39 +0200

    Refacto(keys): migrate to `nestjs-jwks` and remove custom keys module (#41)

--- server/src/auth/auth.module.ts
+++ server/src/auth/auth.module.ts
@@ -5,13 +5,12 @@ import { AuthService } from "@/auth/auth.service";
 import { CookiesService } from "@/auth/cookies.service";
 import { JwtService } from "@/auth/jwt.service";
 import { OidcService } from "@/auth/oidc.service";
-import { KeysModule } from "@/keys/keys.module";
 import { OrganizationModule } from "@/organization/organization.module";
 import { RoleModule } from "@/role/role.module";
 import { UserModule } from "@/user/user.module";
 
 @Module({
-  imports: [KeysModule, OrganizationModule, RoleModule, UserModule],
+  imports: [OrganizationModule, RoleModule, UserModule],
   controllers: [AuthController],
   providers: [AuthService, CookiesService, JwtService, OidcService],
   exports: [JwtService],
