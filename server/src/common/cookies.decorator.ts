dbee8d799e17db4ccae4c1aa799b032a7851e769
Julien Hauseux <julien.hauseux@gmail.com>
2026-06-29 22:09:06 +0200

    Fix formatting

--- server/src/common/cookies.decorator.ts
+++ server/src/common/cookies.decorator.ts
@@ -5,7 +5,6 @@ export const Cookies = createParamDecorator(
   (data: string | undefined, ctx: ExecutionContext): string | undefined => {
     const request = ctx.switchToHttp().getRequest<Request>();
     return (data ? request.cookies[data] : request.cookies) as
-      | string
-      | undefined;
+      string | undefined;
   },
 );
